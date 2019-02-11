30c562e0754d0be353322f867309f05dfceb64a4
verma mukesh <vermamukesh67@gmail.com>
2019-02-11 18:31:33 +0530

    gcd

--- Algorithems/ViewController.swift
+++ Algorithems/ViewController.swift
@@ -14,111 +14,113 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        #if PRODUCTION
-            print("PRODUCTION")
-            self.lblDemo.text = NSLocalizedString("PRODUCTION", comment: "NOT")
-        #else
-            print("STAG")
-            self.lblDemo.text = NSLocalizedString("STAG", comment: "NOT")
-        #endif
+//        // Do any additional setup after loading the view, typically from a nib.
+//        #if PRODUCTION
+//            print("PRODUCTION")
+//            self.lblDemo.text = NSLocalizedString("PRODUCTION", comment: "NOT")
+//        #else
+//            print("STAG")
+//            self.lblDemo.text = NSLocalizedString("STAG", comment: "NOT")
+//        #endif
+//
+//        var foo: Float = 32
+//        var bar: Double = 32
+//
+//        let userLoggedIn: Bool? = false
+//
+//        if !userLoggedIn! {
+//            print("Message one")
+//        } else {
+//            print("Message two")
+//        }
+//
+//        let people = [String](repeating: "Malkovitch", count: 2)
+//        print(people)
+//
+//        let number = 5
+//
+//        switch number {
+//        case 0..<5:
+//            print("First group")
+//        case 5...10:
+//            print("Second group")
+//        case 0...5:
+//            print("Third group")
+//        default:
+//            print("Fourth group")
+//        }
+//
+//        for i in stride(from: 1, to: 17, by: 4) {
+//            print(i)
+//        }
+//
+//        let rounded: Int = Int(round(10.5))
+//        print("Rounding 10.5 makes \(rounded)")
+//
+//        let crew = NSMutableDictionary()
+//        crew.setValue("Kryten", forKey: "Mechanoid")
+//        print(crew.count)
+//
+//        let names = ["Simon", "River", "Book"]
+//
+//        names.forEach {
+//            print($0)
+//        }
+//
+//        let bigNumber = UInt8.max
+//        let biggerNumber = bigNumber
+//        print(biggerNumber)
+//
+//        var crew1 = ["Captain": "Malcolm", "Doctor": "Simon"]
+//        crew1 = [:]
+//        print(crew1.count)
+//
+//        let numbers = [1, 3, 5, 7, 9]
+//
+//        for num in numbers
+//        {
+//            print(num)
+//        }
+//
+//        let result = numbers.filter { $0 >= 5 }
+//        print(result)
+//
+//        func greet(names: String...) {
+//            print("Criminal masterminds:", names.joined(separator: ", "))
+//        }
+//
+//        greet(names: "Malcolm", "Kaylee", "Zoe");
+//
+//        let foo1 = 0x10
+//        print(foo1)
+//
+//        let t1 = Testing(fName: "Verma", lName: "Mukesh", arr:["Verma"])
+//        let t2 = Testing(fName: "Verma", lName: "Mukesh", arr:["Mukesh"])
+//        let t3 = Testing(fName: "Verma", lName: "Mukesh", arr:["Verma"])
+//
+//        if t1 == t2 {
+//            print("both struct are equal")
+//        }
+//        if t1 == t3 {
+//            print("both struct are equal")
+//        }
+//        else
+//        {
+//            print("both struct not are equal")
+//        }
+//
+//        let touple = (1,1)
+//
+//        let arrTouple : [(Int, Int)] = [touple]
+//
+//        print("arrTouple = \(arrTouple)")
+//
+//        let function = makeIncrementer(forIncrement: 10)
+//        print(function())
+//        print(function())
+//        print(function())
         
-        var foo: Float = 32
-        var bar: Double = 32
-        
-        let userLoggedIn: Bool? = false
-        
-        if !userLoggedIn! {
-            print("Message one")
-        } else {
-            print("Message two")
-        }
-        
-        let people = [String](repeating: "Malkovitch", count: 2)
-        print(people)
-        
-        let number = 5
-        
-        switch number {
-        case 0..<5:
-            print("First group")
-        case 5...10:
-            print("Second group")
-        case 0...5:
-            print("Third group")
-        default:
-            print("Fourth group")
-        }
-        
-        for i in stride(from: 1, to: 17, by: 4) {
-            print(i)
-        }
-        
-        let rounded: Int = Int(round(10.5))
-        print("Rounding 10.5 makes \(rounded)")
-        
-        let crew = NSMutableDictionary()
-        crew.setValue("Kryten", forKey: "Mechanoid")
-        print(crew.count)
-        
-        let names = ["Simon", "River", "Book"]
-        
-        names.forEach {
-            print($0)
-        }
-        
-        let bigNumber = UInt8.max
-        let biggerNumber = bigNumber
-        print(biggerNumber)
-        
-        var crew1 = ["Captain": "Malcolm", "Doctor": "Simon"]
-        crew1 = [:]
-        print(crew1.count)
-        
-        let numbers = [1, 3, 5, 7, 9]
-        
-        for num in numbers
-        {
-            print(num)
-        }
-        
-        let result = numbers.filter { $0 >= 5 }
-        print(result)
-        
-        func greet(names: String...) {
-            print("Criminal masterminds:", names.joined(separator: ", "))
-        }
-        
-        greet(names: "Malcolm", "Kaylee", "Zoe");
-        
-        let foo1 = 0x10
-        print(foo1)
-        
-        let t1 = Testing(fName: "Verma", lName: "Mukesh", arr:["Verma"])
-        let t2 = Testing(fName: "Verma", lName: "Mukesh", arr:["Mukesh"])
-        let t3 = Testing(fName: "Verma", lName: "Mukesh", arr:["Verma"])
-        
-        if t1 == t2 {
-            print("both struct are equal")
-        }
-        if t1 == t3 {
-            print("both struct are equal")
-        }
-        else
-        {
-            print("both struct not are equal")
-        }
-        
-        let touple = (1,1)
-        
-        let arrTouple : [(Int, Int)] = [touple]
-        
-        print("arrTouple = \(arrTouple)")
-        
-        let function = makeIncrementer(forIncrement: 10)
-        print(function())
-        print(function())
-        print(function())
+        testGCD()
         
 //        var i = 1
 //
@@ -178,4 +180,25 @@ func makeIncrementer(forIncrement amount: Int) -> () -> Int {
         return runningTotal
     }
     return incrementer
+}
+
+func testGCD()
+{
+    let queue1 = DispatchQueue.global()
+    
+    queue1.async { () -> Void in
+        
+        for i in 1000...1100
+        {
+            print(i)
+        }
+    }
+    
+    queue1.async { () -> Void in
+        
+        for i in 10...110
+        {
+            print(i)
+        }
+    }
 }
